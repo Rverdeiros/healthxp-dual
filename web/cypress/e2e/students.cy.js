1de06fb7fb7b100ba775e9df2883dd0d3d295ece
Raphael Verdeiros <rverdeiros@hotmail.com>
2023-07-23 20:31:21 -0300

    ajustes para publicar o desafio 2

--- web/cypress/e2e/students.cy.js
+++ web/cypress/e2e/students.cy.js
@@ -13,6 +13,7 @@ describe('alunos', () => {
         studentPage.goToRegister()
         studentPage.submitForm(student)
         studentPage.popup.haveText('Dados cadastrados com sucesso.')
+   
     })
 
     it('não deve cadastrar com e-mail duplicado', () => {
@@ -63,7 +64,9 @@ describe('alunos', () => {
         studentPage.requiredMessage('Idade', 'A idade é obrigatória')
         studentPage.requiredMessage('Peso (em kg)', 'O peso é obrigatório')
         studentPage.requiredMessage('Altura', 'A altura é obrigatória')
+   
     })
+    
     it('tentar cadastrar um aluno menor de 16 anos', ()=>{
         const student = students.invalid_age
         
@@ -72,6 +75,7 @@ describe('alunos', () => {
         studentPage.submitForm(student)
         studentPage.requiredMessage('Idade', 'A idade mínima para treinar é 16 anos!')
     })
+    
     it('tentar cadastrar informando peso incorreto', ()=>{
         const student = students.invalid_height
         
@@ -83,6 +87,7 @@ describe('alunos', () => {
         studentPage.requiredMessage('Peso (em kg)', 'O peso deve ser maior do que 0!')
 
     })
+    
     it('tentar cadastrar informando altura incorreta', ()=>{
         const student = students.invalid_height
         
@@ -92,6 +97,7 @@ describe('alunos', () => {
         studentPage.goToRegister()
         studentPage.submitForm(student)
         studentPage.requiredMessage('Altura', 'A altura deve ser maior do que 0!')
+
     })
 
 })
